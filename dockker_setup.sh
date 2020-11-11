353c267dc994b3af3393a5a6db2ac80e8709cea9
Gerry Shona <gshona@ge-g6.kzn.21-school.ru>
2020-11-11 20:51:21 +0300

    auto commit

---
+++ dockker_setup.sh
@@ -0,0 +1,13 @@
+GOINFRE="/goinfre/gshona"
+#export MACHINE_STORAGE_PATH=$GOINFRE/tmp;
+#docker-machine create default;
+#eval $(docker-machine env default);
+​
+mkdir $GOINFRE/Caches
+mv ~/Library/Caches/com.docker.docker $GOINFRE/Caches/
+ln -s $GOINFRE/Caches/com.docker.docker Library/Caches/com.docker.docker
+​
+​
+mkdir $GOINFRE/Containers
+mv ~/Library/Containers/com.docker.docker $GOINFRE/Containers/
+ln -s $GOINFRE/Containers/com.docker.docker Library/Containers/com.docker.docker
