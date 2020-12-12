954ec87a40ab26d866e22763a86781ed3505207d
Gerry Shona <gshona@un-s3.kzn.21-school.ru>
2020-12-12 18:00:06 +0300

    auto commit

--- init.sh
+++ init.sh
@@ -9,8 +9,14 @@ rm -rf ~/Library/Caches/com.docker.docker
 ln -s $GOINFRE/Caches/com.docker.docker ~/Library/Caches/com.docker.docker
 mkdir $GOINFRE/Containers
 mkdir $GOINFRE/.docker
+
 rm -rf ~/.docker
 ln -s $GOINFRE/.docker ~/.docker
+
 mkdir $GOINFRE/Containers/com.docker.docker
 rm -rf ~/Library/Containers/com.docker.docker
 ln -s $GOINFRE/Containers/com.docker.docker ~/Library/Containers/com.docker.docker
+
+rm -rf ~/.minikube
+mkdir $GOINFRE/.minikube
+ln -s $GOINFRE/.minikube ~/.minikube
